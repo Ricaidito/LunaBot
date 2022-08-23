d27b1d7d4e9cc05c1a6188a2ab2e06da324ad062
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-08-23 18:43:42 -0400

    Added avatar command

--- src/utils/message-reply.js
+++ src/utils/message-reply.js
@@ -1,11 +1,6 @@
 const madCatEmoji = ":pouting_cat:";
 
 module.exports = messageReply = message => {
-  if (
-    message.content.toLowerCase() === "owo kiss <@755890198457614398>" ||
-    message.content.toLowerCase().includes("owo kiss <@755890198457614398>")
-  )
-    message.reply(madCatEmoji);
-  else if (message.content.toLowerCase().includes("meow"))
-    message.reply("Meow!");
+  if (message.content.toLowerCase().includes("meow")) message.reply("Meow!");
+  if (message.content.toLowerCase().includes("dog")) message.reply(madCatEmoji);
 };
